b88c6b33886a8cc74c8161671faaf863fa3ac1fc
Connor Bryan <cchromium@gmail.com>
2017-11-11 22:14:38 -0600

    More work on Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Card, Segment } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 import makeClass from 'classnames';
+import {
+  Button,
+  Card,
+  Header,
+  Item,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
 
+/**
+ * @func Home
+ * @desc The home screen provides links to the most intrinsic content,
+ *       as well as the latest news item.
+ * @param {object} props 
+ * @returns {Component}
+ */
 export function Home(props) {
   const { history } = props;
 
   const className = makeClass('text-center fancy');
   const onClick = url => history.push(url);
-
   const explorationCards = [
     {
       className,
@@ -25,18 +39,43 @@ export function Home(props) {
 
   return (
     <Segment.Group>
-      <Segment attached='top'>
+      <Message
+        attached='top'
+        icon='child'
+        header='Welcome to Glassfinder!'
+        content='Take some time to explore out shops and pieces. If you need help, navigate to the "Help" page using the navigation above.'
+        info />
+      <Segment attached='bottom'>
+        <Header
+          as='h2'
+          content='Explore'
+          className='fancy'
+          icon='binoculars' />
         <Card.Group
           items={explorationCards}
           itemsPerRow={2}
           stackable />
       </Segment>
       <Segment attached='bottom'>
+        <Header
+          as='h2'
+          content='Latest update'
+          className='fancy'
+          icon='newspaper' />
+        <Item.Group>
+          <Item
+            className='nested'
+            content='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit eveniet magnam quis quam. Est fugiat veritatis sequi sit dolorem quod consectetur, sunt aperiam, nihil molestias alias, asperiores natus mollitia eaque!'
+            header='Example title'
+            meta='Posted by Connor Bryan on 3/19/1992' />
+        </Item.Group>
       </Segment>
     </Segment.Group>
   );
 }
 
-Home.propTypes = {};
+Home.propTypes = {
+  history: PropTypes.object.isRequired,
+};
 
 export default Home;
