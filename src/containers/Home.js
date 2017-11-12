8e199e67ede09b63023c60db901f902acd579b2a
Connor Bryan <cchromium@gmail.com>
2017-11-11 21:56:36 -0600

    Improvements to Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,39 +1,42 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import {
-  Button,
-  Container,
-  Icon,
-  Segment,
-} from 'semantic-ui-react';
+import React from 'react';
+import { Card, Segment } from 'semantic-ui-react';
+import makeClass from 'classnames';
 
-import CONSTANTS from '../constants';
+export function Home(props) {
+  const { history } = props;
 
-export default class Home extends Component {
-  render() {
-    return (
-      <Container>
-        <Segment.Group>
-          <Segment>
-            <Button.Group fluid vertical>
-              <Button
-                as={Link}
-                className='fancy'
-                fluid
-                to='/explore-shops'>
-                <Icon name={CONSTANTS.ICONS.shop} /> Explore shops
-              </Button>
-              <Button
-                as={Link}
-                className='fancy'
-                fluid
-                to='/explore-pieces'>
-                <Icon name={CONSTANTS.ICONS.piece} /> Explore pieces
-              </Button>
-            </Button.Group>
-          </Segment>
-        </Segment.Group>
-      </Container>
-    );
-  }
+  const className = makeClass('text-center fancy');
+  const onClick = url => history.push(url);
+
+  const explorationCards = [
+    {
+      className,
+      description: 'Explore shops',
+      image: 'https://placehold.it/400x400',
+      onClick: () => onClick('/explore-shops'),
+    },
+    {
+      className,
+      description: 'Explore pieces',
+      image: 'https://placehold.it/400x400',
+      onClick: () => onClick('/explore-pieces'),
+    },
+  ];
+
+  return (
+    <Segment.Group>
+      <Segment attached='top'>
+        <Card.Group
+          items={explorationCards}
+          itemsPerRow={2}
+          stackable />
+      </Segment>
+      <Segment attached='bottom'>
+      </Segment>
+    </Segment.Group>
+  );
 }
+
+Home.propTypes = {};
+
+export default Home;
